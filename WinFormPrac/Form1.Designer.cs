564827fb12147eb4c7bf0b710730422ad76a4935
Jihye Yang <s2127@e-mirim.hs.kr>
2023-05-15 15:10:50 +0900

    공용프로그램

--- WinFormPrac/Form1.Designer.cs
+++ WinFormPrac/Form1.Designer.cs
@@ -28,20 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 150F);
+            this.label1.Location = new System.Drawing.Point(144, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1097, 226);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "기억이안나요";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1410, 744);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WinFormPrac/Form1.cs
+++ WinFormPrac/Form1.cs
@@ -16,5 +16,15 @@ namespace WinFormPrac
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
